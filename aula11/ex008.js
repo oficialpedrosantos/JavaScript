bba78e22831a073e7a1c2fb9331b629c2ec2d58d
oficialpedrosantos <pedrosantosalmeida.henrique@gmail.com>
2024-08-17 11:32:02 -0300

    atvd

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`Você está andando a ${vel}km/hr`)
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Diriga sempre com o cinto de segurança.')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`Morando no ${pais} você é:`)
+if(pais == 'Brasil'){
+    console.log('BRASILEIRO')
+}
+else{
+    console.log('ESTRANGEIRO')
+}
