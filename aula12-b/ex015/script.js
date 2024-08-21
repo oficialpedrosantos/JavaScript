f93f90664444823c09ca8846a6e46d23c7b235e6
oficialpedrosantos <pedrosantosalmeida.henrique@gmail.com>
2024-08-21 20:29:01 -0300

    atvd

--- aula12-b/ex015/script.js
+++ aula12-b/ex015/script.js
@@ -2,6 +2,20 @@ function calcular(){
     var n = new Date
     var data = n.getFullYear()
     var nasc = document.getElementById('txtf')
-    var sex = document.getElementsByName('fsex')
-    
+    var res = document.querySelector('div#res')
+    if (nasc.value.length == 0 || nasc.value > data){
+        alert('[ERRO] Data invalida')
+    } else {
+        var sex = document.getElementsByName('fsex')
+        var idade = data - Number(nasc.value)
+        var genero = ''
+        if (sex[0].checked){
+            genero = 'homem'
+
+             
+        }
+    }
+
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    img.src = 'jovem-m.png'
 }
