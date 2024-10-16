90b8b9f259354841087fbd98f117332fd746da89
oficialpedrosantos <pedrosantosalmeida.henrique@gmail.com>
2024-10-16 14:42:05 -0300

    atvd

--- aula16-b/modelo/java.js
+++ aula16-b/modelo/java.js
@@ -21,8 +21,44 @@ function inLista(n, l) {
 
 function adicionar(){
     if(isNumero(num.value) && inLista(num.value, valores)) {
-        alert('ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        resultado.innerHTML = ''
     } else {
         alert('Valor invalido ou já imposto na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione um valor antes de finalizar!')
+    } else {
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            media = soma / valores.length
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        
+
+
+        resultado.innerHTML = '<p>Resultado:</p>'
+        resultado.innerHTML += `<p>Ao todo temos ${valores.length} números cadastrados.</p>`
+        resultado.innerHTML += `<p>O menor valor adicionado foi: ${menor}</p>`
+        resultado.innerHTML += `<p>O maior valor adicionado foi: ${maior}</p>`
+        resultado.innerHTML += `<p>A soma dos valores foi: ${soma}</p>`
+        resultado.innerHTML += `<p>A media dos valores foi: ${media}</p>`
     }
 }
